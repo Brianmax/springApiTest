b2a2bdccf7eefdfbdcf05e40a9c60302277e60cb
Brianmax <george.maxi@ucsp.edu.pe>
2024-02-21 11:04:17 -0500

    pagination for usuario

--- src/main/java/org/example/springapitest/controller/UsuarioController.java
+++ src/main/java/org/example/springapitest/controller/UsuarioController.java
@@ -1,12 +1,12 @@
 package org.example.springapitest.controller;
 
+import lombok.Getter;
 import org.example.springapitest.document.Usuario;
 import org.example.springapitest.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/usuario")
@@ -17,5 +17,10 @@ public class UsuarioController {
     public Usuario createUsuario(@RequestBody Usuario usuario)
     {
         return usuarioService.createUser(usuario);
+    }
+    @GetMapping("/findAll")
+    public Page<Usuario> findAll(Pageable pageable)
+    {
+        return usuarioService.findAll(pageable);
     }
 }
